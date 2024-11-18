cfa712d45faf1aeb5a322ede56716183c60993f1
Liam Pribis <liam.pribis@digiconsoles.com>
2024-11-18 12:17:30 +0000

    Configure clangd and completion engine

--- lua/mysettings.lua
+++ lua/mysettings.lua
@@ -14,16 +14,40 @@ vim.opt.showmatch = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-
 vim.keymap.set("n", "<Leader>h", "<cmd>nohlsearch<cr>")
+vim.keymap.set("n", "<Bslash>", "<C-^>")
+vim.keymap.set("n", "<Leader>1", "<cmd>Neotree toggle<cr>")
+
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", telescope.find_files, {desc = "find files"})
 
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[syntax on]])
 
 
-vim.keymap.set("n", "<Leader>1", "<cmd>Neotree toggle<cr>")
+local cmp_caps = require ("cmp_nvim_lsp").default_capabilities()
 
-local telescope = require("telescope.builtin")
+local lspconfig = require("lspconfig")
+lspconfig.clangd.setup {
+    capabilities = cmp_caps,
+    root_dir = function(fname)
+        return require("lspconfig.util").root_pattern(
+        "Makefile",
+        "configure.ac",
+        "configure.in",
+        "config.h.in",
+        "meson.build",
+        "meson_options.txt",
+        "build.ninja"
+        )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
 
-vim.keymap.set("n", "<C-p>", telescope.find_files, {desc = "find files"})
+        fname
+        ) or require("lspconfig.util").find_git_ancestor(fname)
+    end,
+    init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+    },
+}
