d9afcbda1d2b8286eaa79fe16ad417c1f509fdc2
Liam Pribis <liam.pribis@digiconsoles.com>
2024-11-18 15:24:51 +0000

    Add copy path option for neotree

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,3 +1,44 @@
+-- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/370
+local function copy_path(state)
+	local node = state.tree:get_node()
+	local filepath = node:get_id()
+	local filename = node.name
+	local modify = vim.fn.fnamemodify
+
+	local results = {
+		filepath,
+		modify(filepath, ":."),
+		modify(filepath, ":~"),
+		filename,
+		modify(filename, ":r"),
+		modify(filename, ":e"),
+	}
+
+
+	vim.ui.select({
+		"1. Absolute path: " .. results[1],
+		"2. Path relative to CWD: " .. results[2],
+		"3. Path relative to HOME: " .. results[3],
+		"4. Filename: " .. results[4],
+		"5. Filename without extension: " .. results[5],
+		"6. Extension of the filename: " .. results[6],
+	}, { prompt = "Choose to copy to clipboard:" }, function(choice)
+		if choice then
+			local i = tonumber(choice:sub(1, 1))
+			if i then
+				local result = results[i]
+				vim.fn.setreg('"', result)
+				vim.notify("Copied: " .. result)
+			else
+				vim.notify("Invalid selection")
+			end
+		else
+
+			vim.notify("Selection cancelled")
+		end
+	end)
+end
+
 return {
     { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
     {
@@ -8,6 +49,11 @@ return {
             "MunifTanjim/nui.nvim",
         },
         opts = {
+            window = {
+                mappings = {
+                    ["Y"] = copy_path,
+                },
+            },
             close_if_last_window = true,
             enable_git_status = true,
             default_component_configs = {
